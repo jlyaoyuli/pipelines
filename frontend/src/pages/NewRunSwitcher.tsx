0ba4ad334db1b0449b6d41015a2d50c281d9ec23
Joe Li <yyjoeli@google.com>
2022-08-25 23:11:37 +0000

    Tiny change to trigger the e2e test.

--- frontend/src/pages/NewRunSwitcher.tsx
+++ frontend/src/pages/NewRunSwitcher.tsx
@@ -96,6 +96,8 @@ function NewRunSwitcher(props: PageProps) {
     }
   }
 
+  // TODO(jlyaoyuli): creating run without pipeline ID and run ID
+
   if (
     runIsFetching ||
     pipelineIsFetching ||
